504b48419f7055afc692625b3b4057d8dfce6dd2
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-09-20 14:05:00 -0600

    Donate route

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { IndexRoute, Route } from 'react-router';
 import Layout from './components/Layout/Layout';
+import DonateContainer from './containers/Donate/DonateContainer';
 import HomeContainer from './containers/Home/HomeContainer';
 import JoinUsContainer from './containers/JoinUs/JoinUsContainer';
 
 const routes = (
   <Route path="/" component={Layout}>
     <IndexRoute component={HomeContainer} />
+    <Route path="/donate" component={DonateContainer} />
     <Route path="/join-us" component={JoinUsContainer} />
   </Route>
 );
