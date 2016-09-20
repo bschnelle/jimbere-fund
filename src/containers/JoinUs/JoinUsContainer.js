ce674971c0152fbdd44842d76c060c5821e101e6
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-09-20 14:01:37 -0600

    JoinUsContainer and JoinUs shells

---
+++ src/containers/JoinUs/JoinUsContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import JoinUs from '../../components/JoinUs/JoinUs';
+
+const JoinUsContainer = () => (
+  <JoinUs />
+);
+
+export default JoinUsContainer;
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { IndexRoute, Route } from 'react-router';
 import Layout from './components/Layout/Layout';
 import HomeContainer from './containers/Home/HomeContainer';
+import JoinUsContainer from './containers/JoinUs/JoinUsContainer';
 
 const routes = (
   <Route path="/" component={Layout}>
     <IndexRoute component={HomeContainer} />
+    <Route path="/join-us" component={JoinUsContainer} />
   </Route>
 );
 
