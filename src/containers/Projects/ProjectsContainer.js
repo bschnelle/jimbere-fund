d7f5afc7c0118e32b77fbd8ae984244f3ce295c1
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-09-20 14:07:24 -0600

    ProjectsContainer, Projects, and Projects route

---
+++ src/containers/Projects/ProjectsContainer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Projects from '../../components/Projects/Projects';
+
+const ProjectsContainer = () => (
+  <Projects />
+);
+
+export default ProjectsContainer;
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import Layout from './components/Layout/Layout';
 import DonateContainer from './containers/Donate/DonateContainer';
 import HomeContainer from './containers/Home/HomeContainer';
 import JoinUsContainer from './containers/JoinUs/JoinUsContainer';
+import ProjectsContainer from './containers/Projects/ProjectsContainer';
 
 const routes = (
   <Route path="/" component={Layout}>
     <IndexRoute component={HomeContainer} />
     <Route path="/donate" component={DonateContainer} />
     <Route path="/join-us" component={JoinUsContainer} />
+    <Route path="/projects" component={ProjectsContainer} />
   </Route>
 );
 
