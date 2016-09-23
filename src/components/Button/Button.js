f1dce9d4df60b221f8df5d353ca72e0a8e226589
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-09-23 09:17:49 -0600

    removed loader from Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,27 +3,14 @@ import { Link } from 'react-router';
 import classes from './Button.scss';
 
 const Button = (props) => {
-  /* call props.onClick unless we're already fetching more posts */
-  const onClick = () => { !props.loading && props.onClick(); };
   let className = classes.button;
   if (props.className) className += ` ${props.className}`;
-
-  let loader;
-  if (typeof props.loading !== 'undefined') {
-    let loaderClassName = classes.loader;
-    if (props.loading) {
-      className += ` ${classes.busy}`;
-      loaderClassName += ` ${classes.animate}`;
-    }
-    loader = <div className={loaderClassName} />;
-  }
 
   return props.to
   ? <Link className={className} to={props.to}>{props.label}</Link>
   : (
-    <div className={className} onClick={onClick}>
-      {loader}
-      <span className={classes.label}>{props.label}</span>
+    <div className={className} onClick={props.onClick}>
+      {props.label}
     </div>
   );
 };
@@ -31,7 +18,6 @@ const Button = (props) => {
 Button.propTypes = {
   className: PropTypes.string,
   label: PropTypes.string.isRequired,
-  loading: PropTypes.bool,
   onClick: PropTypes.func,
   to: PropTypes.string
 };
