5b5674a6bc7a621434fd4e0029681a106452c6f9
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-09-23 13:58:09 -0600

    Contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import classes from './Contact.scss';
 
 const Contact = () => (
-  <div>
-    <h1>Contact</h1>
-    <p>917-224-2095</p>
+  <div className={classes.contact}>
+    <div className={classes.overlay} />
+    <div className={classes.greeting}>
+      <h1>Contact Us</h1>
+    </div>
+    <div className={classes.info}>
+      <h3>917.224.2095 | obadiasndaba@gmail.com</h3>
+    </div>
   </div>
 );
 
