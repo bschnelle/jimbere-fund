d1c0684b0dbc060521fe5c2b09149671aefc9d9f
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-09-26 20:17:02 -0600

    Donate

--- src/components/Donate/Donate.js
+++ src/components/Donate/Donate.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import Button from '../Button/Button';
 import classes from './Donate.scss';
 
 const Donate = () => (
   <div className={classes.donate}>
-    <div className={classes.info}>
-      <h1>Donate</h1>
-    </div>
-    <div className={classes.widget}>
-
+    <div>
+      <div>
+        <div>
+          <h1>Donate</h1>
+          <p>Congo is among the most destitute countries on Earth.</p>
+          <p>You can help change that.</p>
+        </div>
+      </div>
+      <div>
+        <Button label="Give Now" />
+      </div>
     </div>
   </div>
 );
