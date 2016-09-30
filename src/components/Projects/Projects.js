121581e8186f2baa16b78e569ea2b38d56e2d20e
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-09-30 16:39:05 -0600

    Fact, Projects, Team styles

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -19,10 +19,8 @@ class Projects extends Component {
     return (
       <div className={classes.projects}>
         <div>
-          <div>
-            <h1>Projects</h1>
-            <h6>Coming soon!</h6>
-          </div>
+          <h1>Projects</h1>
+          <h6>Coming soon!</h6>
         </div>
       </div>
     );
