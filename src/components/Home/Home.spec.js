b0026259e9e29b1e9488cc4880af065e4701fbe8
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-10-04 11:25:44 -0600

    Home tests

--- src/components/Home/Home.spec.js
+++ src/components/Home/Home.spec.js
@@ -1,12 +1,58 @@
-/* import React from 'react';
+import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import { Link } from 'react-router';
 import Home from './Home';
+import HomeApproach from '../HomeApproach/HomeApproach';
+import HomeFacts from '../HomeFacts/HomeFacts';
+import HomeLanding from '../HomeLanding/HomeLanding';
+import HomeProcess from '../HomeProcess/HomeProcess';
+import HomePurpose from '../HomePurpose/HomePurpose';
+import classes from './Home.scss';
 
 describe('Home', () => {
-  it('renders a Link with "Go to Counter!"', () => {
+  it('renders a div with a class of .home', () => {
     const wrapper = shallow(<Home />);
-    expect(wrapper.contains(<Link to="/counter">Go to Counter!</Link>)).to.be.true;
+    expect(wrapper.type()).to.equal('div');
+    expect(wrapper.is(`.${classes.home}`)).to.be.true;
   });
-}); */
+
+  describe('nested components', () => {
+    let wrapper;
+
+    beforeEach(() => { wrapper = shallow(<Home />); });
+
+    describe('HomeLanding', () => {
+      it('has props.onScrollClick of type function', () => {
+        expect(typeof wrapper.find(HomeLanding).prop('onScrollClick')).to.equal('function');
+      });
+    });
+
+    describe('HomeFacts', () => {
+      it('has props.id equal to "facts"', () => {
+        expect(wrapper.find(HomeFacts).prop('id')).to.equal('facts');
+      });
+    });
+
+    describe('HomePurpose', () => {
+      it('renders', () => {
+        expect(wrapper.find(HomePurpose)).to.have.length(1);
+      });
+    });
+
+    describe('HomeApproach', () => {
+      it('has is passed props.isSmall of the same name', () => {
+        const isSmall = true;
+        wrapper.setProps({ isSmall });
+        expect(wrapper.find(HomeApproach).prop('isSmall')).to.equal(isSmall);
+      });
+    });
+
+    describe('HomeProcess', () => {
+      it('has is passed props.isSmall of the same name', () => {
+        const isSmall = true;
+        wrapper.setProps({ isSmall });
+        expect(wrapper.find(HomeProcess).prop('isSmall')).to.equal(isSmall);
+      });
+    });
+  });
+});
