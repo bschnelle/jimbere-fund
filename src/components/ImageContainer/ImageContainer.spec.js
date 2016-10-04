83bf0616087e7ead5214700cb5296ddb4627a83d
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-10-04 15:26:59 -0600

    ImageContainer tests

--- src/components/ImageContainer/ImageContainer.spec.js
+++ src/components/ImageContainer/ImageContainer.spec.js
@@ -1,19 +1,54 @@
-/* import React from 'react';
+import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import Footer from './Footer';
-import classes from './Footer.scss';
+import ImageContainer from './ImageContainer';
+import classes from './ImageContainer.scss';
 
-describe('Footer', () => {
-  it('renders a root div with a .footer class', () => {
-    const wrapper = shallow(<Footer />);
-    expect(wrapper.type()).to.equal('div');
-    expect(wrapper.is(`.${classes.footer}`)).to.be.true;
+const image = {};
+const src = '/path/to/image.jpg';
+
+describe('ImageContainer', () => {
+  before(() => { global.Image = () => image; });
+  after(() => delete global.Image);
+
+  describe('constructor()', () => {
+    it('sets state.backgroundImage to props.src via Image.onload', () => {
+      const wrapper = shallow(<ImageContainer src={src} />);
+      image.onload();
+      expect(wrapper.state('backgroundImage')).to.equal(src);
+    });
   });
 
-  it('contains a div with a .social class', () => {
-    const wrapper = shallow(<Footer />);
-    expect(wrapper.find(`.${classes.social}`)).to.have.length(1);
+  describe('render()', () => {
+    it('renders a root div with a .imageContainer class', () => {
+      const wrapper = shallow(<ImageContainer src={src} />);
+      expect(wrapper.is(`.${classes.imageContainer}`)).to.be.true;
+    });
+
+    it('if defined, adds props.className to root div', () => {
+      const className = 'myClass';
+      const wrapper = shallow(<ImageContainer className={className} src={src} />);
+      expect(wrapper.is(`.${className}`)).to.be.true;
+    });
+
+    it('if defined, adds props.imageClassName to first nested div', () => {
+      const imageClassName = 'myImageClass';
+      const wrapper = shallow(<ImageContainer imageClassName={imageClassName} src={src} />);
+      expect(wrapper.childAt(0).prop('className')).to.equal(imageClassName);
+    });
+
+    it('if state.backgroundImage is defined style to backgroundImage is set to its value', () => {
+      const backgroundImage = '/path/to/image.jpg';
+      const wrapper = shallow(<ImageContainer src={src} />);
+      expect(wrapper.childAt(0).prop('style')).to.be.undefined;
+      wrapper.setState({ backgroundImage });
+      expect(wrapper.childAt(0).prop('style').backgroundImage).to.equal(`url(${backgroundImage})`);
+    });
+
+    it('renders its children', () => {
+      const child = <div>child</div>;
+      const wrapper = shallow(<ImageContainer src={src}>{child}</ImageContainer>);
+      expect(wrapper.contains(child)).to.be.true;
+    });
   });
 });
-*/
