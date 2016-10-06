9ad3f3f33ee5b2996ce4dd6bd7f5fb3b5001f822
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-10-05 18:16:04 -0600

    fixed named/default export errors in routes

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import { IndexRoute, Route } from 'react-router';
-import LayoutContainer from './containers/Layout/LayoutContainer';
-import BlogContainer from './containers/Blog/BlogContainer';
-import BlogPostContainer from './containers/BlogPost/BlogPostContainer';
-import BlogPostListContainer from './containers/BlogPostList/BlogPostListContainer';
-import ContactContainer from './containers/Contact/ContactContainer';
-import DonateContainer from './containers/Donate/DonateContainer';
-import HomeContainer from './containers/Home/HomeContainer';
-import GetInvolvedContainer from './containers/GetInvolved/GetInvolvedContainer';
-import ProjectsContainer from './containers/Projects/ProjectsContainer';
-import SignUpContainer from './containers/SignUp/SignUpContainer';
-import TeamContainer from './containers/Team/TeamContainer';
+import LayoutCont from './containers/Layout/LayoutContainer';
+import BlogCont from './containers/Blog/BlogContainer';
+import BlogPostCont from './containers/BlogPost/BlogPostContainer';
+import BlogPostListCont from './containers/BlogPostList/BlogPostListContainer';
+import ContactCont from './containers/Contact/ContactContainer';
+import DonateCont from './containers/Donate/DonateContainer';
+import HomeCont from './containers/Home/HomeContainer';
+import GetInvolvedCont from './containers/GetInvolved/GetInvolvedContainer';
+import ProjectsCont from './containers/Projects/ProjectsContainer';
+import SignUpCont from './containers/SignUp/SignUpContainer';
+import TeamCont from './containers/Team/TeamContainer';
 
 // TODO create/move to UI Service
 const onChange = () => { document.body.scrollTop = document.documentElement.scrollTop = 0; };
 
 const routes = (
-  <Route path="/" component={LayoutContainer} onChange={onChange}>
-    <IndexRoute component={HomeContainer} />
-    <Route path="/blog" component={BlogContainer}>
-      <IndexRoute component={BlogPostListContainer} />
-      <Route path="/blog/:slug" component={BlogPostContainer} />
+  <Route path="/" component={LayoutCont} onChange={onChange}>
+    <IndexRoute component={HomeCont} />
+    <Route path="/blog" component={BlogCont}>
+      <IndexRoute component={BlogPostListCont} />
+      <Route path="/blog/:slug" component={BlogPostCont} />
     </Route>
-    <Route path="/contact" component={ContactContainer} />
-    <Route path="/donate" component={DonateContainer} />
-    <Route path="/get-involved" component={GetInvolvedContainer} />
-    <Route path="/projects" component={ProjectsContainer} />
-    <Route path="/sign-up" components={{ simple: SignUpContainer }} />
-    <Route path="/team" component={TeamContainer} />
+    <Route path="/contact" component={ContactCont} />
+    <Route path="/donate" component={DonateCont} />
+    <Route path="/get-involved" component={GetInvolvedCont} />
+    <Route path="/projects" component={ProjectsCont} />
+    <Route path="/sign-up" components={{ simple: SignUpCont }} />
+    <Route path="/team" component={TeamCont} />
   </Route>
 );
 
