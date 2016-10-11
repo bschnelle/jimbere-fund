526d8e0562a74e3ce56b5f7db74b841f9c576be5
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-10-11 09:01:41 -0600

    BlogPost not found placeholder

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -2,16 +2,14 @@ import React, { PropTypes } from 'react';
 import { Map as iMap } from 'immutable';
 import classes from './BlogPost.scss';
 
-const BlogPost = (props) => (
-  <div className="blogPostWrapper">
-    <div>
-      <h4>{props.post.get('title')}</h4>
-      <div
-        dangerouslySetInnerHTML={{ __html: props.post.get('content') }}
-      />
-    </div>
+const BlogPost = (props) => (!props.post
+? <h6 className={classes.notFound}>Hmm. We can't seem to find that post.</h6>
+: (
+  <div>
+    <h4>{props.post.get('title')}</h4>
+    <div dangerouslySetInnerHTML={{ __html: props.post.get('content') }} />
   </div>
-);
+));
 
 BlogPost.propTypes = {
   post: PropTypes.instanceOf(iMap)
