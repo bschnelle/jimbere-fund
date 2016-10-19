325650d5a1ba1fbc58e9d6b2b4f954c43a86a4d5
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-10-19 15:48:44 -0600

    GetInvolvedFundraising tests

--- src/components/GetInvolvedFundraising/GetInvolvedFundraising.spec.js
+++ src/components/GetInvolvedFundraising/GetInvolvedFundraising.spec.js
@@ -1,60 +1,80 @@
-/* import React from 'react';
+import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import Button from '../Button/Button';
 import Container from '../Container/Container';
-import ImageContainer from '../ImageContainer/ImageContainer';
-import { GetInvolved } from './GetInvolved';
-import classes from './GetInvolved.scss';
+import { GetInvolvedFundraising } from './GetInvolvedFundraising';
+import classes from './GetInvolvedFundraising.scss';
 
-describe('GetInvolved', () => {
+describe('GetInvolvedFundraising', () => {
   let wrapper;
-  beforeEach(() => { wrapper = shallow(<GetInvolved />); });
+  beforeEach(() => { wrapper = shallow(<GetInvolvedFundraising />); });
 
-  describe('render()', () => {
-    describe('root element', () => {
-      it('is of type ImageContainer', () => {
-        expect(wrapper.type()).to.equal(ImageContainer);
+  describe('renders a root Container', () => {
+    let container;
+    beforeEach(() => {
+      container = wrapper.find(Container);
+    });
+
+    describe('with props', () => {
+      it('id = "gi-fundraising"', () => {
+        expect(container.prop('id')).to.equal('gi-fundraising');
       });
 
-      describe('props', () => {
-        it('className = .imageContainer', () => {
-          expect(wrapper.prop('className')).to.equal(classes.imageContainer);
-        });
+      it('section = true', () => {
+        expect(container.prop('section')).to.be.true;
+      });
 
-        it('src = "/images/new_york-o.jpg"', () => {
-          expect(wrapper.prop('src')).to.equal('/images/new_york-o.jpg');
-        });
+      it('title = "Fundraising"', () => {
+        expect(container.prop('title')).to.equal('Fundraising');
       });
     });
 
-    describe('Container', () => {
-      let container;
-      beforeEach(() => { container = wrapper.find(Container); });
-
-      describe('props', () => {
-        it('className = .container', () => {
-          expect(container.prop('className')).to.equal(classes.container);
-        });
-
-        it('fluid = true', () => {
-          expect(container.prop('fluid')).to.be.true;
-        });
-
-        it('title = "Get Involved"', () => {
-          expect(container.prop('title')).to.equal('Get Involved');
-        });
-      });
-    });
-
-    describe('nested elements', () => {
-      it('contains an h6', () => {
-        expect(wrapper.find('h6')).to.have.length(1);
+    describe('with nested components', () => {
+      let fundraising;
+      beforeEach(() => {
+        fundraising = wrapper.find(`.${classes.fundraising}`);
       });
 
-      it('contains a div with a .positions class', () => {
-        expect(wrapper.find(`.${classes.positions}`)).to.have.length(1);
+      it('wrapped in a div with a .fundraising class', () => {
+        expect(fundraising.type()).to.equal('div');
+      });
+
+      it('of four paragraphs', () => {
+        expect(fundraising.find('p')).to.have.length(4);
+      });
+
+      describe('of one Button with props of', () => {
+        let button;
+        beforeEach(() => {
+          button = fundraising.find(Button);
+        });
+
+        it('href = "https://www.razoo.com/story/new?subject=Jimbere-Fund"', () => {
+          expect(button.prop('href')).to.equal('https://www.razoo.com/story/new?subject=Jimbere-Fund');
+        });
+
+        it('label = "Start Fundraising"', () => {
+          expect(button.prop('label')).to.equal('Start Fundraising');
+        });
+      });
+
+      describe('when props.isSmall is false', () => {
+        it('contains three h4 elements', () => {
+          expect(fundraising.find('h4')).to.have.length(3);
+        });
+
+        it('contains one img element', () => {
+          expect(fundraising.find('img')).to.have.length(1);
+        });
+      });
+
+      describe('when props.isSmall is true', () => {
+        it('contains three img elements', () => {
+          wrapper.setProps({ isSmall: true });
+          expect(wrapper.find(`.${classes.fundraising}`).find('img')).to.have.length(3);
+        });
       });
     });
   });
 });
-*/
