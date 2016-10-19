9ea0dfdb219ec33f19dba21bd65be359a84cc8f1
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-10-19 16:04:26 -0600

    fixed/added BlogPostContainer tests

--- src/containers/BlogPost/BlogPostContainer.spec.js
+++ src/containers/BlogPost/BlogPostContainer.spec.js
@@ -1,29 +1,54 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 import { fromJS } from 'immutable';
 import BlogPost from '../../components/BlogPost/BlogPost';
 import { BlogPostContainer, stateToProps } from './BlogPostContainer';
 
-const post = fromJS({ slug: 'post-1' });
-
 describe('BlogPostContainer', () => {
+  let getPosts;
+  let loading;
+  let params;
+  let post;
+  let wrapper;
+
+  beforeEach(() => {
+    getPosts = sinon.stub();
+    loading = false;
+    params = {};
+    post = fromJS({ slug: 'post-1' });
+    wrapper = shallow(
+      <BlogPostContainer
+        getPosts={getPosts}
+        loading={loading}
+        params={params}
+        post={post}
+      />
+    );
+  });
+
   describe('render()', () => {
     it('renders a BlogPost component', () => {
-      const wrapper = shallow(<BlogPostContainer />);
       expect(wrapper.type()).to.equal(BlogPost);
     });
 
+    it('passes props.loading to BlogPost', () => {
+      expect(wrapper.prop('loading')).to.equal(loading);
+    });
+
     it('passes props.post to BlogPost', () => {
-      const wrapper = shallow(<BlogPostContainer post={post} />);
       expect(wrapper.prop('post')).to.equal(post);
     });
   });
 
   describe('stateToProps()', () => {
-    it('find the blog.posts with a slog matching props.params.slug', () => {
-      const state = {
+    let state;
+    let props;
+    beforeEach(() => {
+      state = {
         blog: fromJS({
+          loading: true,
           posts: [
             { slug: 'post-2' },
             { slug: 'post-3' },
@@ -31,7 +56,14 @@ describe('BlogPostContainer', () => {
           ]
         })
       };
-      const props = { params: { slug: 'post-1' } };
+      props = { params: { slug: 'post-1' } };
+    });
+
+    it('maps state.blog.loading to props.loading', () => {
+      expect(stateToProps(state, props).loading).to.equal(state.blog.get('loading'));
+    });
+
+    it('maps the blog post with slug of props.params.slug to props.post', () => {
       expect(stateToProps(state, props).post).to.equal(post);
     });
   });
