968c74c157a3983a9fcf88c85fee2be9a8b58204
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-10-19 18:47:21 -0600

    some Donate tests

--- src/components/Donate/Donate.spec.js
+++ src/components/Donate/Donate.spec.js
@@ -7,51 +7,73 @@ import ImageContainer from '../ImageContainer/ImageContainer';
 import classes from './Donate.scss';
 
 describe('Donate', () => {
-  it('renders a root div', () => {
-    const wrapper = shallow(<Donate />);
-    expect(wrapper.type()).to.equal('div');
+  describe('componentDidMount()', () => {
+    beforeEach(() => {
+      shallow(<Donate />).instance().componentDidMount();
+    });
+
+    it('sets window.r_params to an object', () => {
+      expect(typeof window.r_params).to.equal('object');
+    });
+
+    it('sets window.r_protocol based on document.location.protocol', () => {
+      expect(/https?:\/\//.test(window.r_protocol)).to.be.true;
+    });
+
+    it('sets window.r_identifier to "Jimbere-Fund"', () => {
+      expect(window.r_identifier).to.equal('Jimbere-Fund');
+    });
+
+    // TODO more tests
   });
 
-  describe('ImageContainer props', () => {
-    let imageContainer;
-
-    beforeEach(() => {
+  describe('render()', () => {
+    it('renders a root div', () => {
       const wrapper = shallow(<Donate />);
-      imageContainer = wrapper.find(ImageContainer);
+      expect(wrapper.type()).to.equal('div');
     });
 
-    it('className = .imageContainer', () => {
-      expect(imageContainer.prop('className')).to.equal(classes.imageContainer);
-    });
+    describe('ImageContainer props', () => {
+      let imageContainer;
 
-    it('src = "/images/school-o.jpg"', () => {
-      expect(imageContainer.prop('src')).to.equal('/images/school-o.jpg');
-    });
-  });
-
-  describe('Container', () => {
-    let container;
-
-    beforeEach(() => {
-      const wrapper = shallow(<Donate />);
-      container = wrapper.find(Container);
-    });
-
-    it('has a class of .donate', () => {
-      expect(container.is(`.${classes.donate}`)).to.be.true;
-    });
-
-    it('has a title prop equal to "Donate"', () => {
-      expect(container.prop('title')).to.equal('Donate');
-    });
-
-    describe('nested elements/components', () => {
-      it('contains an h6', () => {
-        expect(container.find('h6')).to.have.length(1);
+      beforeEach(() => {
+        const wrapper = shallow(<Donate />);
+        imageContainer = wrapper.find(ImageContainer);
       });
 
-      it('contains a p', () => {
-        expect(container.find('p')).to.have.length(1);
+      it('className = .imageContainer', () => {
+        expect(imageContainer.prop('className')).to.equal(classes.imageContainer);
+      });
+
+      it('src = "/images/school-o.jpg"', () => {
+        expect(imageContainer.prop('src')).to.equal('/images/school-o.jpg');
+      });
+    });
+
+    describe('Container', () => {
+      let container;
+
+      beforeEach(() => {
+        const wrapper = shallow(<Donate />);
+        container = wrapper.find(Container);
+      });
+
+      it('has a class of .donate', () => {
+        expect(container.is(`.${classes.donate}`)).to.be.true;
+      });
+
+      it('has a title prop equal to "Donate"', () => {
+        expect(container.prop('title')).to.equal('Donate');
+      });
+
+      describe('nested elements/components', () => {
+        it('contains an h6', () => {
+          expect(container.find('h6')).to.have.length(1);
+        });
+
+        it('contains a p', () => {
+          expect(container.find('p')).to.have.length(1);
+        });
       });
     });
   });
