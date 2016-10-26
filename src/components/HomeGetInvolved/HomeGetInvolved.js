dfee050578dbc040b955f023c703b2f22c866520
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-10-25 20:57:43 -0600

    changed HomeGetInvolved to functional stateless component

--- src/components/HomeGetInvolved/HomeGetInvolved.js
+++ src/components/HomeGetInvolved/HomeGetInvolved.js
@@ -1,37 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Container from '../Container/Container';
 import GetInvolvedTiles from '../GetInvolvedTiles/GetInvolvedTiles';
 import ImageContainer from '../ImageContainer/ImageContainer';
 import classes from './HomeGetInvolved.scss';
 
-/**
- * story section of root route
- */
-class HomeGetInvolved extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { readMore: false };
-    this.toggleReadMore = this.toggleReadMore.bind(this);
-  }
-
-  /**
-   * toggleReadMore - open/close detail
-   */
-  toggleReadMore() {
-    this.setState({ readMore: !this.state.readMore });
-  }
-
-  render() {
-    return (
-      <ImageContainer className={classes.imageContainer} src="/images/hands-o.jpg">
-        <Container className={classes.section} container fluid section title="Get Involved">
-          <p>We need your time and talents.</p>
-          <GetInvolvedTiles link />
-        </Container>
-      </ImageContainer>
-    );
-  }
-}
+const HomeGetInvolved = () => (
+  <ImageContainer className={classes.imageContainer} src="/images/hands-o.jpg">
+    <Container className={classes.section} container fluid section title="Get Involved">
+      <p>We need your time and talents.</p>
+      <GetInvolvedTiles />
+    </Container>
+  </ImageContainer>
+);
 
 export default HomeGetInvolved;
