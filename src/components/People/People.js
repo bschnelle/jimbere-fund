f4a2b1d58e812df1fcba6335d78658e8c9a4c4dd
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-12-18 13:23:46 -0700

    people header and profiles

--- src/components/People/People.js
+++ src/components/People/People.js
@@ -1,29 +1,69 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { Component } from 'react';
+import Button from '../Button/Button';
 import Container from '../Container/Container';
+import ImageContainer from '../ImageContainer/ImageContainer';
 import PeopleProfile from '../PeopleProfile/PeopleProfile';
+import secondaryTheme from '../../containers/secondaryTheme/secondaryTheme';
 import classes from './People.scss';
 import people from './members';
 
 /**
  * /people route component
  */
-const People = () => {
-  return (
-    <Container className={classes.people} title="People">
-      <div>
-        <h6>Our team of incredible individuals</h6>
-        {people.map((group) => (
-          <div className={classes.group} key={group.name}>
-            <h4>{group.name}</h4>
-            {group.members.map((member) => (
-              <PeopleProfile {...member} />
-            ))}
-          </div>
-        ))}
-      </div>
-    </Container>
-  );
-};
+class People extends Component {
 
-export default People;
+  constructor(props) {
+    super(props);
+    this.groups = ['Staff', 'US Board', 'DRC Board'];
+    this.state = { selectedGroup: 'Staff' };
+    this.changeGroup = this.changeGroup.bind(this);
+  }
+
+  changeGroup(e) {
+    this.setState({ selectedGroup: e.target.innerHTML });
+  }
+
+  render() {
+    const { selectedGroup } = this.state;
+
+    return (
+      <div>
+        <ImageContainer
+          className={classes.imageContainer}
+          imageClassName={classes.image}
+          src="/images/office-o.jpg"
+        >
+          <Container className={classes.header} fluid title="People">
+            <h6>Our team of incredible individuals</h6>
+          </Container>
+        </ImageContainer>
+        <div className={classes.buttons}>
+          {this.groups.map(group =>
+            <Button
+              flat={group !== selectedGroup}
+              key={group}
+              label={group}
+              onClick={this.changeGroup}
+            />)
+          }
+        </div>
+        <Container className={classes.section} container section title={selectedGroup}>
+          <div className={classes.people}>
+            <div className={classes.group}>
+              <div className={classes.members}>
+                {people[selectedGroup].map((member, index) => (
+                  <div className={classes.member} key={index}>
+                    <PeopleProfile {...member} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default secondaryTheme(People);
--- src/components/PeopleProfile/PeopleProfile.js
+++ src/components/PeopleProfile/PeopleProfile.js
@@ -1,16 +1,22 @@
 import React, { PropTypes } from 'react';
 import classes from './PeopleProfile.scss';
 
-const PeopleProfile = ({ image, name, onClick, title }) => (
-  <div className={classes.profile} onClick={onClick}>
-    <img alt={name} src={image} />
-    <h5>{name}</h5>
-    <span>{title}</span>
-  </div>
-);
+const PeopleProfile = ({ image, name, onClick, title }) => {
+  let imageSrc = image ? `/images/${image}` : '/images/peoplePlaceholder.jpg';
+
+  return (
+    <div className={classes.profile} disabled={!!onClick} onClick={onClick}>
+      <div className={classes.imageWrapper}>
+        <img alt={name} src={imageSrc} />
+      </div>
+      <span className={classes.name}>{name}</span>
+      <span className={classes.title}>{title}</span>
+    </div>
+  );
+};
 
 PeopleProfile.propTypes = {
-  image: PropTypes.string.isRequired,
+  image: PropTypes.string,
   name: PropTypes.string.isRequired,
   onClick: PropTypes.func,
   title: PropTypes.string.isRequired
