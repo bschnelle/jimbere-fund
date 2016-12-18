6eb531f92e0d4f7dafe72e5b3c7d9ae8db6df318
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2016-12-18 11:27:47 -0700

    updated bios

---
+++ src/components/People/members.js
@@ -0,0 +1,251 @@
+const members = [
+  {
+    name: 'Staff',
+    members: [
+      {
+        name: 'Obadias Ndaba',
+        title: 'Founder and CEO',
+        bio: `
+          Obadias is Founder and CEO of Jimbere Fund. He’s a seasoned non-profit executive with
+          over 8 years of experience in nonprofit management, international development,
+          fundraising, analytical writing and research management. He has worked in micro-finance
+          and commercial banking in Rwanda, and in nonprofit management and think tanks in Kenya
+          and the United States. Obadias regularly writes on development issues, African affairs
+          and economic trends. His articles and views have appeared in The Huffington Post, The
+          Voice of America, The New York Times, Libre Afrique, The African Executive and The
+          Africa Review, among other publications. He holds an Executive Master’s in Development
+          Policies and Practices from The Graduate Institute Geneva, a certificate in Nonprofit
+          Financial Management from Harvard Kennedy School of Government and an Economics degree
+          from Kigali Independent University. Obadias has extensively travelled and lectured on
+          global issues in over 25 countries in five continents.
+        `
+      },
+      {
+        name: 'Adele Kibasumba',
+        title: 'Co-founder and Senior Advisor',
+        bio: `
+          Adele is Co-founder and Senior Advisor at Jimbere Fund, where she focusses on access to
+          healthcare and women’s empowerment. She’s an activist who has advocated for human rights
+          in the Democratic Republic of Congo over the past five years. She has spoken out on
+          violence against women and protection of minorities in conflict zones, including at the
+          United Nations, the State Department and the White House. Adele and her family survived
+          the 2004 Gatumba massacre, which took her sister’s life and left several of her friends
+          and family members dead or wounded. This experience informs her advocacy and sparked
+          interest in public service. She studied at Roberts Wesleyan College in Upstate New York
+          and works at New York Presbyterian Weil Cornell.
+        `
+      },
+      {
+        name: 'Sandra Uwiringiyimana',
+        title: 'Co-founder and Director of Partnerships & Communications',
+        bio: `
+          Sandra is Co-founder and Director of Partnerships & Communications at Jimbere Fund,
+          where she focusses on mobilizing people and institutions to support our mission. She’s
+          the author of How Dare The Sun Rise, a memoir on surviving war in Congo and navigating
+          America as a refugee. Originally from the Democratic Republic of Congo, Sandra and her
+          family were forced to flee the country due to the ongoing conflicts. In 2004, Sandra and
+          her family were refugees in Burundi when their camp was attacked and left 166 people
+          died, including Sandra’s sister. Since her family’s resettlement in 2007, Sandra has
+          fought hard to raise awareness and call for Justice for the Gatumba massacre and other
+          human rights abuses in the region. She has spoken at the United Nations, Women in the
+          World and many other gatherings. She’s passionate about girls’ education and ending
+          child marriage.
+        `
+      },
+      {
+        name: 'Benjamin Itangishaka',
+        title: 'Project Manager',
+        bio: `
+          Ben is a Project Manager at Jimbere Fund, where he focusses on program design and
+          development. Ben originally came from Sud-Kivu in Democratic Republic of Congo and
+          studied Political Science and Sociology at the University of Northern Colorado where
+          he focused on multicultural social issues and politics in developing countries. He
+          is currently pursuing a M.S. in Global Affairs at New York University, with a
+          concentration in international development and humanitarian assistance. Prior to his
+          graduate studies at NYU, he spent three years working with at-risk youths in Denver,
+          Colorado and assisting them with various peer-mentoring programs. Ben teaches
+          Kayaking, Standup Paddleboard and Canoeing, and loves playing soccer.
+        `
+      },
+      {
+        name: 'Justin Ruhumuriza',
+        title: 'Community Coordinator',
+        bio: `
+          Justin is Community Coordinator at Jimbere Fund, where he focusses on working with
+          communities to identify their most pressing needs and their solutions. He has worked
+          with remote communities in Fizi and Mwenga territories in South Kivu for the past 6
+          years. He was the coordinator of Action pour le Développement et la Paix au Sud-Kivu,
+          a community organization working to bring about change in the high plateaus of Itombwe,
+          for the past five years. He also for worked for Transcultural Psycho-Social Organization
+          in Minembwe and served as accountant for Minembwe health district. Justin holds a
+          bachelor’s degree in Development Studies from Institut Superieur de Développement
+          Rural in Bukavu.
+        `
+      }
+    ]
+  },
+  {
+    name: 'U.S. Board of Directors',
+    members: [
+      {
+        name: 'Moses Nkosilathi Vuma',
+        title: 'Chair',
+        bio: `
+          Moses holds a Masters degree in Public Administration and Finance from New York
+          University. He founded Khuluma Center, a non-profit organization which creates
+          platforms for global cultural exchange. A committed public servant, he previously
+          worked for the Metropolitan Transportation Authority of New York City where he
+          accumulated extensive experience in public finance management and auditing. Prior
+          to that, Moses was an analyst with Pygma Consulting, focused on increasing investment
+          flows to Africa. He’s currently an entrepreneur in real estate market in New York.
+        `
+      },
+      {
+        name: 'Hemant Wandhwani',
+        title: 'Treasurer',
+        bio: `
+          Hemant started his career working at Diversified Investment Advisors where he built
+          financial applications for the pension industry. He founded and leads the Hanuman
+          Capital Initiative, an investment firm that works to bridge the funding gap in global
+          development and local urban economic development needs. In 2000, he co-founded
+          Translation City, a language translation business servicing Fortune 500 companies,
+          NGOs, and government agencies. Hemant advises and sits on boards of numerous
+          organizations, including Asian American Health and Human Services, Save Latin America,
+          Newark's Sumei Arts Center, Harlem/South Bronx Youthbuild mentorship programs, and the
+          United South Asian Donor Registry. He has also been appointed to a number of commissions
+          and advisory committees by the Governor and state legislature of New Jersey. Hemant
+          has a B.A. in Economics from Manhattanville College and an M.S. in Management with a
+          specialization in International Business from New Jersey Institute of Technology.
+        `
+      },
+      {
+        name: 'Rachel Henderson',
+        title: 'Secretary',
+        bio: `
+          Rachel is an American writer, radio host and activist. Through her love for world
+          experience, she has embraced the countries of Ghana, Rwanda and Israel.  Most recently
+          she has extended her role as “voice” in both film and the visual arts, using each
+          platform as an aid against injustice. Rachel finds that our most passionate forms of
+          expression come from the ones that guide us from within. Rachel has a degree of
+          Bachelor Arts in Communication from Roberts Wesleyan College.
+        `
+      },
+      {
+        name: 'Adele Kibasumba',
+        title: 'Member',
+        bio: `
+          Adele is Co-founder and Senior Advisor at Jimbere Fund, where she focusses on access
+          to healthcare and women’s empowerment. She’s an activist who has advocated for human
+          rights in the Democratic Republic of Congo over the past five years. She has spoken
+          out on violence against women and protection of minorities in conflict zones,
+          including at the United Nations, the State Department and the White House. Her advocacy
+          work helped create the Office of the U.S Special Envoy for the Great Lakes Region and
+          the Democratic Republic of Congo at the State Department. Adele’s life experiences
+          surviving the 2004 Gatumba massacre, which took her sister’s life and 166 other refuges
+          in a refugee camp in Burundi, informs her advocacy and sparked interest in public
+          service. She studied at Roberts Wesleyan College in Upstate New York and works at New
+          York Presbyterian Weil Cornell.
+        `
+      },
+      {
+        name: 'Benjamin Schnelle',
+        title: 'Member',
+        bio: `
+          Ben is a software engineer based out of Denver, CO. He previously worked as an analyst
+          in both the energy and insurance industries. He is a CPA, and holds degrees in
+          accounting and finance. He is passionate about bleeding edge technology and finding
+          ways to apply those innovations in a meaningful way. He’s built Jimbere Fund’s website
+          and provides advice on technology. He loves the outdoors, art, architecture, video
+          games, craft beer, and coffee.
+        `
+      },
+      {
+        name: 'Obadias Ndaba',
+        title: 'Ex-officio member',
+        bio: `
+          Obadias is Founder and CEO of Jimbere Fund. He’s a seasoned non-profit executive with
+          over 8 years of experience in nonprofit management, international development,
+          fundraising, analytical writing and research management. He has worked in micro-finance
+          and commercial banking in Rwanda, and in nonprofit management and think tanks in Kenya
+          and the United States. Obadias regularly writes on development issues, African affairs
+          and economic trends. His articles and views have appeared in The Huffington Post, The
+          Voice of America, The New York Times, Libre Afrique, The African Executive and The Africa
+          Review, among other publications. He holds an Executive Master’s in Development Policies
+          and Practices from The Graduate Institute Geneva, a certificate in Nonprofit Financial
+          Management from Harvard Kennedy School of Government and an Economics degree from Kigali
+          Independent University. Obadias has extensively travelled and lectured on global issues
+          in over 25 countries in five continents. Follow him @ObadiasNdaba
+        `
+      },
+      {
+        name: 'JD Stier',
+        title: 'Member'
+      },
+      {
+        name: 'Paul Freeman',
+        title: 'Member'
+      }
+    ]
+  },
+  {
+    name: 'D.R.C. Board of Directors',
+    members: [
+      {
+        name: 'Adele Kibasumba',
+        title: 'Chair',
+        bio: `
+          Adele is Co-founder and Senior Advisor at Jimbere Fund, where she focusses on access to
+          healthcare and women’s empowerment. She’s an activist who has advocated for human rights
+          in the Democratic Republic of Congo over the past five years. She has spoken out on
+          violence against women and protection of minorities in conflict zones, including at the
+          United Nations, the State Department and the White House. Her advocacy work helped create
+          the Office of the U.S Special Envoy for the Great Lakes Region and the Democratic
+          Republic of Congo at the State Department. Adele’s life experiences surviving the 2004
+          Gatumba massacre, which took her sister’s life and 166 other refuges in a refugee camp
+          in Burundi, informs her advocacy and sparked interest in public service. She studied at
+          Roberts Wesleyan College in Upstate New York and works at New York Presbyterian Weil
+          Cornell.
+        `
+      },
+      {
+        name: 'Paul Ntare',
+        title: 'Treasurer',
+        bio: `
+          Paul has more than 25 years of experience in human rights, development and the judiciary
+          in the Great Lakes Region. He has worked on police and justice reforms for many years,
+          and advised policymakers on human rights. He led development projects for Food for the
+          Hungry International in the region in mid 2000s, an experience that left a mark on him
+          and sparked further interest in development. Currently an independent lawyer, Paul is
+          passionate about the rule of law and its overlooked role on development.
+        `
+      },
+      {
+        name: 'Muhamiriza Neza Aline',
+        title: 'Secretary'
+      },
+      {
+        name: 'Lazare R. Sebitereko',
+        title: 'Member',
+        bio: `
+          Lazare is President and Rector of Eben-Ezer University in DRC, where he is a professor of
+          community development and theology. He’s a Board member of Faculty Journal (HTS)
+          University of Pretoria, and a Visiting scholar at Cornell University, Hebrew University,
+          Ruppin College (Israel) and Africa Hope University (Burundi). Lazare is Co-founder of
+          Eeben-Ezer Ministry International, Eben-Ezer University, Floresta Burundi and Burundi
+          American International Academy. He also consults with Geneva Global Inc. (USA), Help
+          African Child (Netherlands), Global Food Crisis Fund (USA) and Gigawatt Global
+          (Netherlands), among others. Lazare holds a PhD in Political Theology from the
+          University of Pretoria and a Diploma in peace building from Eastern Mennonite University,
+          Virginia. He has published numerous works on theology, politics, anthropology and
+          community development.
+        `
+      },
+      {
+        name: 'Gasore Zébédée',
+        title: 'Member'
+      }
+    ]
+  }
+];
+
+export default members;
