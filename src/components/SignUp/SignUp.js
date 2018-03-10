e38645dd2fe43ca4daf700ebaa3807d5cee7dd97
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2018-03-10 09:03:02 -0700

    update/fix Get Involved

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -8,16 +8,39 @@ import classes from './SignUp.scss';
 class SignUp extends Component {
 
   componentDidMount() {
-    const script = document.createElement('script');
-    script.src = 'https://s3-eu-west-1.amazonaws.com/share.typeform.com/embed.js';
-    document.body.appendChild(script);
+    /* eslint-disable */
+    let qs,
+      js,
+      q,
+      s,
+      d = document,
+      gi = d.getElementById,
+      ce = d.createElement,
+      gt = d.getElementsByTagName,
+      id = 'typef_orm',
+      b = 'https://embed.typeform.com/';
+    if (!gi.call(d, id)) {
+      js = ce.call(d, 'script');
+      js.id = id;
+      js.src = `${b}embed.js`;
+      q = gt.call(d, 'script')[0];
+      q.parentNode.insertBefore(js, q);
+    }
+    /* eslint-enable */
   }
 
   render() {
-    const formUrl = 'https://forms.typeform.io/to/Vm8snKvsEu';
+    // const formUrl = 'https://forms.typeform.io/to/Vm8snKvsEu';
     return (
       <div className={classes.signUp}>
-        <iframe id="typeform-full" frameBorder={0} src={formUrl} />
+        <div
+          className="typeform-widget"
+          data-url="https://jimberefund.typeform.com/to/ryZ55E"
+          data-transparency="50"
+          data-hide-headers
+          data-hide-footer
+          style={{ width: '100%' }}
+        />
       </div>
     );
   }
