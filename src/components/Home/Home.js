e67a95d0839c7df36ed074db36d78ace73c6e1c5
Benjamin Schnelle <benjamin.schnelle@gmail.com>
2018-03-10 09:56:15 -0700

    update HomeApproach

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,6 @@ import React, { PropTypes } from 'react';
 import HomeApproach from '../HomeApproach/HomeApproach';
 import HomeGetInvolved from '../HomeGetInvolved/HomeGetInvolved';
 import HomeLanding from '../HomeLanding/HomeLanding';
-import HomeProcess from '../HomeProcess/HomeProcess';
 import HomeWork from '../HomeWork/HomeWork';
 import secondaryTheme from '../../containers/secondaryTheme/secondaryTheme';
 import classes from './Home.scss';
@@ -18,7 +17,6 @@ export const Home = (props) => {
       <HomeLanding isMedium={isMedium} />
       <HomeWork />
       <HomeApproach isSmall={isSmall} />
-      <HomeProcess isSmall={isSmall} />
       <HomeGetInvolved />
     </div>
   );
